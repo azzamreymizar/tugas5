b024b039900a1bf0c653fe8d8f1f394216adf264
azzamreymizar <areymizar@gmail.com>
2025-04-28 07:55:50 +0700

    tugas 5 azzam

---
+++ tugas5.php
@@ -0,0 +1,9 @@
+<?php
+$nilai_matematika = 65;
+$nilai_bahasa = 55;
+
+if ($nilai_matematika >= 80 && $nilai_bahasa >= 70){
+    echo "selamat! kamu boleh nonton bioskop akhir pekan ini!";
+} else {
+    echo "Yah... belajar lagi ya, lain kali bisa!";
+}
