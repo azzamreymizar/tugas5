a0016e9f335f6524a7bd2dad25b6f5eaca75a910
azzamreymizar <areymizar@gmail.com>
2025-05-14 10:25:48 +0700

    Add files via upload

---
+++ tugas10.php
@@ -0,0 +1,8 @@
+<?php 
+
+$i = 1;
+
+while ($i < 7) {
+    echo "Azzam Arditia Reymizar </br> </br>";
+    $i++;
+}
---
+++ tugas11.php
@@ -0,0 +1,7 @@
+<?php
+ 
+ $i = 1;
+ do {
+    echo "Azzam Arditia Reymizar </br> </br>";
+    $i++;
+ } while ($i < 7);                
