9da90cfe667e62e5dbc0e0138306b1058afd0e08
azzamreymizar <areymizar@gmail.com>
2025-05-15 13:04:22 +0700

    Add files via upload

---
+++ tugas12.php
@@ -0,0 +1,35 @@
+<!-- Array      $buku = ["Informatika", "Pai", "Pplg", "tahfiz"];
+
+                $buku = Array ("Informatika","Pai","pplg","Tahfidz"); -->
+
+                <!-- echo "$buku[2]" -->
+
+<?php
+
+// $hewan = ["Singa","Monyet","Kucing","Sapi","Semut","Udang","ikan"];
+// $Merk = array ("Samsung","vivo","iphone","xiaomi","advan", "nubia" );
+
+// echo "$hewan[3]";
+
+// $nilai = [78,88,83,67,93];
+// echo "Nilai PPLG = $nilai[1]</br>";
+// echo "Nilai Bahasa Arab = $nilai[4]</br>";
+ 
+
+// ARRAY ASOSIATIF  
+
+// $mahasiswa = [
+//     "nama" => "Azzam",
+//     "Nim" => "54321",
+//     "Prodi" => "Bahasa Arab",
+//     "Alamat" => "Jalan goa ria sudiang no 13",
+// ];
+
+// echo $mahasiswa ["Alamat"];
+
+
+$merklokal = ["Compass","Ortus","Aerostreet","Specs"];
+echo "Daftar Koleksi Sepatu </br>";
+foreach ($merklokal as $DaftarKoleksiSepatu) {
+    echo "-" .$DaftarKoleksiSepatu ."</br>";
+}
